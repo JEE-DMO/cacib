7b962833d6de83f69f335a96311a9e68e4a16a73
Djamel MOUCHENE <mouchene.djamel@gmail.com>
2024-06-14 20:20:24 +0200

    initial import

---
+++ bale4-api/src/main/java/fr/cacib/bale4/bale4_api/Bale4ApiApplication.java
@@ -0,0 +1,13 @@
+package fr.cacib.bale4.bale4_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bale4ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bale4ApiApplication.class, args);
+	}
+
+}
---
+++ bale4-api/src/test/java/fr/cacib/bale4/bale4_api/Bale4ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cacib.bale4.bale4_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bale4ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
